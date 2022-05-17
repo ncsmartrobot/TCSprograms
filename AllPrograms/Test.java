5cbf37823f1fa8db143c0db13af464ab8ecbe74a
unknown <ncsmartrobot@gmail.com>
2022-05-17 07:11:33 +0000

    First Day Program

---
+++ AllPrograms/Test.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+class AddTwoNumbes {
+void SumOfTwoNumbers() {
+    int number1 = 10;
+    int number2 = 20;
+    int result = number1 + number2;
+    System.out.print("Sum of two numbers " + result);
+}
+}
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AddTwoNumbes object = new AddTwoNumbes();// object of this class -> AddTwoNumber
+	    object.SumOfTwoNumbers();// calling from here
+
+	}
+
+}
+
+
